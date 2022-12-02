f52046fe04c97de9927777304c18e7ce715ec8e9
Toxa Kniotee <toxa.kniotee@protonmail.com>
2022-12-02 13:59:05 -0500

    Inline tests in spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,9 @@
-import Day2Tests as Day2
+import Day2
 import Test.HUnit
 
 main = runTestTTAndExit $ TestList 
-  [ TestLabel "Day 2" Day2.tests
+  [ TestLabel "Day 2" $ TestList
+    [ TestCase $ Day2.part1 >>= assertEqual "Part 1" 11449
+    , TestCase $ Day2.part2 >>= assertEqual "Part 2" 13187
+    ]
   ]
