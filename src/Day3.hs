40c9abc866242fc0ddc672114c8c4c54f20583fe
Toxa Kniotee <toxa.kniotee@protonmail.com>
2022-12-03 04:41:39 -0500

    Day 3

---
+++ src/Day3.hs
@@ -0,0 +1,58 @@
+module Day3
+  ( sample1
+  , sample2
+  , part1
+  , part2
+  ) where
+
+import Data.List (sort)
+import Data.Maybe (fromJust)
+
+sample1 :: IO Int
+sample1 = part1' <$> readFile "sample/day3.txt"
+
+sample2 :: IO Int
+sample2 = part2' <$> readFile "sample/day3.txt"
+
+part1 :: IO Int
+part1 = part1' <$> readFile "input/day3.txt"
+
+part2 :: IO Int
+part2 = part2' <$> readFile "input/day3.txt"
+
+part1' :: String -> Int
+part1' = sum . (priority . common . halfs <$>) . lines
+
+part2' :: String -> Int
+part2' =  sum . (priority <$>) . (findCommon <$>) . (sort <$$>) . chunks 3 <$> lines
+
+halfs :: [a] -> ([a], [a])
+halfs xs = splitAt (length xs `div` 2) xs
+
+common :: Ord a => ([a], [a]) -> a
+common (as, bs) = findCommon [as', bs']
+  where
+    as' = sort as
+    bs' = sort bs
+
+findCommon :: (Ord c) => [[c]] -> c
+findCommon xss
+  | all (== minHead) (heads xss) = minHead
+  | otherwise = findCommon $ dropWhile (== minHead) <$> xss
+  where
+    heads = map head
+    minHead = minimum . heads $ xss
+
+priority :: Char -> Int
+priority a = fromJust $ lookup a $ zip chars [1,2..]
+  where
+    chars = ['a'..'z'] ++ ['A' .. 'Z']
+
+chunks :: Int -> [a] -> [[a]]
+chunks _ [] = []
+chunks n xs = left : chunks n right
+  where
+    (left, right) = splitAt 3 xs
+
+(<$$>) :: (Functor f1, Functor f2) => (a -> b) -> f1 (f2 a) -> f1 (f2 b)
+a <$$> b = (a <$>) <$> b
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,16 @@
 import Day2
+import Day3
 import Test.HUnit
 
 main = runTestTTAndExit $ TestList 
   [ TestLabel "Day 2" $ TestList
     [ TestCase $ Day2.part1 >>= assertEqual "Part 1" 11449
     , TestCase $ Day2.part2 >>= assertEqual "Part 2" 13187
+    ]
+  , TestLabel "Day 3" $ TestList
+    [ TestCase $ Day3.sample1 >>= assertEqual "Sample 1" 157
+    , TestCase $ Day3.sample2 >>= assertEqual "Sample 2" 70
+    , TestCase $ Day3.part1 >>= assertEqual "Part 1" 7691
+    , TestCase $ Day3.part2 >>= assertEqual "Part 2" 2508
     ]
   ]
