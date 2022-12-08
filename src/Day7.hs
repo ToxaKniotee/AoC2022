607bfc58c37e9c4cb52c631a9d9f53611bac21ab
Toxa Kniotee <toxa.kniotee@protonmail.com>
2022-12-08 09:30:10 -0500

    TEMP: Start refactoring day7 solution
    As of now this solution does not work due to the parser. Needs checking
    to see why the parser is not working

--- src/Day7.hs
+++ src/Day7.hs
@@ -2,71 +2,52 @@ module Day7
   ( part1
   , part2
   ) where
-import Text.Parsec (Parsec, many, digit, char, newline, string, parse, try)
+import Text.Parsec (Parsec, many, digit, newline, string, parse, try, choice, parserTraced)
 import Text.Parsec.Char (anyChar)
 import Text.Parsec.Combinator (manyTill)
 import Text.ParserCombinators.Parsec ((<|>))
-import Data.Tree (Tree(Node, rootLabel), flatten, rootLabel)
-import Data.List (sort, foldl1')
+import Data.Tree (Tree(Node), flatten)
+import Data.List (sort)
+import Debug.Trace (trace)
 
 data Command
   = Cd String
-  | Ls [LsEntry]
-  deriving (Show)
-
-data LsEntry
-  = File Int String
-  | Directory String
+  | Ls Int
   deriving (Show)
 
 type Folder = Tree (String, Int)
 
-getFileSize :: LsEntry -> Int
-getFileSize (File n _) = n
-getFileSize _ = 0
-
-addSubDirectory :: Folder -> Folder -> Folder
-addSubDirectory child (Node (pn, ps) sub) = Node (pn, newSize) (child:sub)
-  where
-    newSize = (snd . rootLabel) child + ps
-
-addFile :: Int -> Folder -> Folder
-addFile s' (Node (n, s) sub) = Node (n, s + s') sub
-
+-- part1 :: IO Int
 part1 :: IO Int
-part1 = findSubDirs 100000 . (`calculateDirs` []) . parseInput <$> readFile "input/day7.txt"
-  where
-    findSubDirs minSize (Node _ children) = sum . filter (<= minSize) . allSubFolders $ children
+part1 = sum . filter (<= 100000) . tail . parseInput <$> readFile "input/day7.txt"
 
 part2 :: IO Int
-part2 = findDirToDelete . (`calculateDirs` []) . parseInput <$> readFile "input/day7.txt"
+part2 = f . parseInput <$> readFile "input/day7.txt"
   where
-    findDirToDelete (Node (_, usedSpace) dirs) = head . dropWhile (isNotEnoughSpace usedSpace) . sort . allSubFolders $ dirs
-    isNotEnoughSpace usedSpace folderSize = usedSpace - folderSize + 30000000 > 70000000
+    f [] = error "Emtpy Items"
+    f (x:xs) = head . dropWhile (isNotEnough x) . reverse . sort $ xs
+    isNotEnough currentUse folderSize = currentUse - folderSize + 30000000 > 70000000
 
 calculateDirs :: [Command] -> [Folder] -> Folder
 -- No Commands left to process, single folder (root)
 calculateDirs [] [folder] = folder
--- Commands available
-calculateDirs (command:commands) dirStack = case command of
-  Cd ".." -> calculateDirs commands updatedStack
-    where
-      updatedStack = newHead:folders
-      (left, folders) = splitAt 2 dirStack
-      newHead = foldl1' addSubDirectory left
-  Cd newDir -> calculateDirs commands $ Node (newDir, 0) [] : dirStack
-  Ls files -> calculateDirs commands (newParent : tail dirStack)
-    where
-      filesSize = sum $ getFileSize <$> files
-      newParent = addFile filesSize (head dirStack)
 -- No commands left, folders still left in the stack
 calculateDirs [] folders = calculateDirs [Cd ".."] folders 
+-- Commands available
+calculateDirs (command:commands) dirStack = case command of
+  Cd ".." -> calculateDirs commands .foldOne $ dirStack
+  Cd newDir -> calculateDirs commands . (createTree newDir:) $ dirStack
+  -- Ls bytes -> calculateDirs commands $ mapHead (mapValue (mapSize (+ bytes))) dirStack
+  Ls bytes -> calculateDirs commands . addSize' bytes $ dirStack
+  where
+    addSize' b' (Node (n, b) fs' : ns) = Node (n, b + b') fs' : ns
+    addSize' _ _ = error "Attempt to modify non existing folder"
+    createTree n = Node (n, 0) []
+    foldOne ( child@(Node (_, s') _) : Node (pn, ps) pc :stack ) = Node (pn, ps + s') (child:pc) : stack
+    foldOne _ = error "Invalid configuration for fold "
 
-allSubFolders :: [Tree (a, b)] -> [b]
-allSubFolders children = (snd <$>) $ children >>= flatten
-
-parseInput :: String -> [Command]
-parseInput = liftParser inputParser
+parseInput :: String -> [Int]
+parseInput = map snd . flatten . (`calculateDirs` []) . liftParser inputParser
 
 liftParser :: Parsec String () a -> String -> a
 liftParser p i = case parse p "" i of
@@ -77,11 +58,11 @@ inputParser :: Parsec String () [Command]
 inputParser = many commands
   where
     commands = try cd <|> try ls
-    cd = string "$ cd " >> Cd <$> folderName
-    folderName = manyTill anyChar newline
-    ls = string "$ ls" >> newline >> Ls <$> many files
+    cd = parserTraced "cd" $ string "$ cd " >> (Cd <$> folderName)
+    folderName = parserTraced "folderName" $ manyTill anyChar newline
+    ls = parserTraced "ls" $ string "$ ls" >> newline >> (Ls <$> (sum <$> many files))
     files = try file <|> try dir
-    file = File <$> number <*> fileName
-    fileName = char ' ' >> manyTill anyChar newline
-    dir = string "dir " >> Directory <$> manyTill anyChar newline
-    number = read <$> many digit
+    file = parserTraced "fileSize" $ number <* manyTill anyChar newline
+    dir = parserTraced "dir" $ string "dir " >> manyTill anyChar newline >> pure 0
+    number = (p "Number" <$>) $ parserTraced "numbr" $ read <$> many digit
+    p t a = trace (t <> show a) a
